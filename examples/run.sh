7fe1d68f9b6bfd3b6de35bb7a6cc2d1e326b5761
Harsh Sinha <mail.sinha.harsh@gmail.com>
2022-05-30 11:21:51 -0400

    Issues solved: Return Dataset from CLI

--- examples/run.sh
+++ examples/run.sh
@@ -10,7 +10,7 @@
 
  INPUT_DIR="/media/harsh/My Passport/MRI_Datasets/sinhah-20220520_201328/"
  OUTPUT_DIR="/home/harsh/My Passport/MRI_Datasets/metadata/"
- python /home/harsh/PycharmProjects/MRdataset/MRdataset/cli.py --dataroot "$INPUT_DIR" --metadataroot "$OUTPUT_DIR" --style xnat --name cha_mjff --verbose -c
+ python /home/harsh/PycharmProjects/MRdataset/MRdataset/cli.py --dataroot "$INPUT_DIR" --metadataroot "$OUTPUT_DIR" --style xnat --name cha_mjff -c
 
 #INPUT_DIR="/media/harsh/My Passport/MRI_Datasets/sinhah-20220520_210659/"
 #OUTPUT_DIR="/home/harsh/PycharmProjects/MRdataset/MRdataset/resources/"

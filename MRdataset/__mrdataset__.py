63161109fe59f23f0e1ed3bc85ead01e9a95725e
Harsh Sinha <harsh.sinha@pitt.edu>
2022-07-03 11:02:05 -0400

    Added entry point

---
+++ MRdataset/__mrdataset__.py
@@ -0,0 +1,16 @@
+from sys import version_info
+
+if version_info.major > 2:
+    from MRdataset import cli
+else:
+    raise NotImplementedError("MRdataset requires Python 3 or higher. "
+                              "Upgrade to Python 3+ or use [virtual] environments.")
+
+
+def main():
+    """Entry point"""
+    cli.main()
+
+
+if __name__ == '__main__':
+    main()

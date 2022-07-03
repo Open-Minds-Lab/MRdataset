26ebb6e3203510a356f99d68f35c836a893e8c38
Harsh Sinha <harsh.sinha@pitt.edu>
2022-07-03 10:59:01 -0400

    Improved required and optional arguments in CLI

--- MRdataset/cli.py
+++ MRdataset/cli.py
@@ -12,37 +12,25 @@ def main():
     required = parser.add_argument_group('required arguments')
     optional = parser.add_argument_group('optional arguments')
 
-    # Add help
+    required.add_argument('-d', '--data_root', type=str, required=True,
+                          help='directory containing downloaded dataset with dicom files, supports nested hierarchies')
+    required.add_argument('-s', '--style', type=str, required=True,
+                          help='choose type of dataset, one of [xnat|bids|other]')
+
+    optional.add_argument('-n', '--name', type=str,
+                          help='provide a identifier/name for dataset')
     optional.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
                           help='show this help message and exit')
-    required.add_argument('-i', '--dataroot', type=str,
-                          help='directory containing downloaded dataset with dicom files, supports nested hierarchies')
-    required.add_argument('-m', '--metadataroot', type=str,
-                          help='directory to store metadata files')
-    required.add_argument('-n', '--name', type=str,
-                          help='provide a identifier/name for dataset')
     optional.add_argument('-r', '--reindex', action='store_true',
                           help='overwrite existing metadata files')
     optional.add_argument('-v', '--verbose', action='store_true',
                           help='allow verbose output on console')
-    optional.add_argument('-c', '--create', action='store_true',
-                          help='create directories if required')
-    required.add_argument('-s', '--style', type=str,
-                          help='choose type of dataset, one of [xnat|bids|other]')
 
     args = parser.parse_args()
-    if not Path(args.dataroot).is_dir():
-        raise OSError('Expected valid directory for --dataroot argument, Got {0}'.format(args.dataroot))
-    metadata_dir = Path(args.metadataroot)
-    if not metadata_dir.is_dir():
-        if args.create:
-            metadata_dir.mkdir(parents=True, exist_ok=True)
-        else:
-            raise OSError('Expected valid directory for --metadata argument. '
-                          'Use -c flag to create new directories automatically')
+    if not Path(args.data_root).is_dir():
+        raise OSError('Expected valid directory for --data_root argument, Got {0}'.format(args.data_root))
 
     dataset = create_dataset(style=args.style, data_root=args.dataroot, verbose=args.verbose)
-
     return dataset
 
 

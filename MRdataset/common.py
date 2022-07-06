07569638241dfdd1e7702d7ffd7862f81de3e5e3
Harsh Sinha <harsh.sinha@pitt.edu>
2022-07-06 11:34:53 -0400

    import parsing functions from protocol_compliance

--- MRdataset/common.py
+++ MRdataset/common.py
@@ -1,15 +1,10 @@
+from pathlib import Path
+
+import pydicom
 from MRdataset.utils import config
 from nibabel.nicom import csareader
 from pydicom.multival import MultiValue
 import logging
-
-
-def get_dicom_property(dicom, attribute):
-    element = dicom.get(getattr(config, attribute), None)
-    if not element:
-        return None
-    return element.value
-
 
 def get_project(dicom):
     return get_dicom_property(dicom, 'STUDY')
@@ -88,3 +83,147 @@ def header_exists(dicom):
     except Exception as e:
         logging.exception(e)
         return False
+
+
+def parse(dicom_path):
+    filepath = Path(dicom_path)
+    if not filepath.exists():
+        raise OSError("Expected a valid filepath, Got invalid path : {0}\n"
+                      "Consider re-indexing dataset.".format(filepath))
+
+    try:
+        dicom = pydicom.dcmread(filepath,
+                                stop_before_pixels=True)
+    except OSError:
+        raise FileNotFoundError(
+            "Unable to read dicom file from disk : {0}".format(filepath)
+        )
+
+    subj = SubjectNode(filepath)
+    for k in config.PARAMETER_TAGS.keys():
+        value = get_param_value_by_name(dicom, k)
+        # the value should be hashable
+        # a dictionary will be used later to count the majority value
+        if not functional.is_hashable(value):
+            value = str(value)
+        subj.params[k] = value
+
+    csa_values = csa_parser(dicom)
+    subj.params["slice_order"] = config.SODict[csa_values['so']]
+    subj.params['ipat'] = csa_values['ipat']
+    subj.params['shim'] = csa_values['shim']
+
+    subj.params["is3d"] = get_param_value_by_name(dicom, "mr_acquisition_type") == '3D'
+    subj.params["modality"] = "_".join([
+        str(get_header(dicom, "series_number")),
+        get_header(dicom, "series_description")]).replace(" ", "_")
+    subj.params["effective_echo_spacing"] = effective_echo_spacing(dicom)
+    subj.params["phase_encoding_direction"] = get_phase_encoding(dicom,
+                                                                 is3d=subj.params['is3d'],
+                                                                 echo_train_length=subj.params['echo_train_length'])
+    return subj
+
+
+def get_param_value_by_name(dicom, name):
+    data = dicom.get(config.PARAMETER_TAGS[name], None)
+    if data:
+        return data.value
+    return None
+
+
+def get_header(dicom, name):
+    data = dicom.get(config.HEADER_TAGS[name], None)
+    if data:
+        return data.value
+    return None
+
+
+def csa_parser(dicom):
+    series_header = csareader.read(get_header(dicom, 'series_header_info'))
+    items = functional.safe_get(series_header, 'tags.MrPhoenixProtocol.items')
+    if items:
+        text = items[0].split("\n")
+    else:
+        raise FileNotFoundError
+
+    start = False
+    end = False
+    props = {}
+    for e in text:
+        if e[:15] == '### ASCCONV END':
+            end = True
+        if start and not end:
+            ele = e.split()
+            if ele[1].strip() == "=":
+                props[ele[0]] = ele[2]
+        if e[:17] == '### ASCCONV BEGIN':
+            start = True
+
+    so = props.get("sKSpace.ucMultiSliceMode", None)
+    ipat = props.get("sPat.lAccelFactPE", None)
+    shim = props.get("sAdjData.uiAdjShimMode", None)
+    return {
+        'so': so,
+        'ipat': ipat,
+        'shim': shim
+    }
+
+
+def effective_echo_spacing(dicom):
+    # if self.get("echo_train_length") > 1: # Check if etl == pel
+    #     check = (self.get("echo_train_length") == self.get("phase_encoding_lines"))
+    #     if not check:
+    #         print("PhaseEncodingLines is not equal to EchoTrainLength : {0}".format(self.filepath))
+    bwp_phase_encode = get_param_value_by_name(dicom, 'bwp_phase_encode')
+    phase_encoding = get_param_value_by_name(dicom, 'phase_encoding_lines')
+
+    if (bwp_phase_encode is None) or (phase_encoding is None):
+        return None
+    else:
+        value = 1000 / (
+            bwp_phase_encode * phase_encoding)
+
+        # Match value to output of dcm2niix
+        return value / 1000
+
+
+def get_phase_encoding(dicom, is3d, echo_train_length, is_flipy=True):
+    """
+    https://github.com/rordenlab/dcm2niix/blob/23d087566a22edd4f50e4afe829143cb8f6e6720/console/nii_dicom_batch.cpp
+    """
+    is_skip = False
+    if is3d:
+        is_skip = True
+    if echo_train_length > 1:
+        is_skip = False
+    image_header = get_header(dicom, 'image_header_info')
+    phvalue = functional.safe_get(image_header, 'tags.PhaseEncodingDirectionPositive.items')
+    if phvalue:
+        phpos = phvalue[0]
+    else:
+        return None
+
+    ped_dcm = get_param_value_by_name(dicom, "phase_encoding_direction")
+
+    ped = ""
+    assert ped_dcm in ["COL", "ROW"]
+    if not is_skip and ped_dcm == "COL":
+        ped = "j"
+    elif not is_skip and ped_dcm == "ROW":
+        ped = "i"
+    if phpos >= 0 and not is_skip:
+        if phpos == 0 and ped_dcm == 'ROW':
+            ped += "-"
+        elif ped_dcm == "COL" and phpos == 1 and is_flipy:
+            ped += "-"
+        elif ped_dcm == "COL" and phpos == 0 and not is_flipy:
+            ped += "-"
+        ped_dict = {
+            'i': 'Left-Right',
+            'i-': 'Right-Left',
+            'j-': 'Anterior-Posterior',
+            'j': 'Posterior-Anterior'
+        }
+        return ped_dict[ped]
+    else:
+        return None
--- MRdataset/config.py
+++ MRdataset/config.py
@@ -9,8 +9,55 @@ SERIES_NUMBER = (0x20, 0x11)
 PROTOCOL_NAME = (0x18, 0x1030)
 SEQUENCE_NAME = (0x18, 0x24)
 IMAGE_TYPE = (0x08, 0x08)
-IMAGE_HEADER_INFO = (0x29, 0x1010)
-SERIES_HEADER_INFO = (0x29, 0x1020)
 ECHO_NUMBER = (0x18, 0x86)
+
 # Constant Paths
 CACHE_DIR = ".mrdataset"
+PARAMETER_TAGS = {
+    "manufacturer": [0x08, 0x70],
+    "organ": [0x18, 0x15],
+    "te": [0x18, 0x81],
+    "tr": [0x18, 0x80],
+    "b0": [0x18, 0x87],
+    "flip_angle": [0x18, 0x1314],
+    "bwpx": [0x18, 0x95],
+    "echo_train_length": [0x18, 0x91],
+    "scanning_sequence": [0x18, 0x20],
+    "sequence_variant": [0x18, 0x21],
+    "mr_acquisition_type": [0x18, 0x23],
+    "phase_encoding_lines": [0x18, 0x89],
+    "bwp_phase_encode": [0x19, 0x1028],
+    "phase_encoding_direction": [0x18, 0x1312],
+
+}
+HEADER_TAGS = {
+    "image_header_info": [0x29, 0x1010],
+    "series_header_info": [0x29, 0x1020],
+    "series_description": [0x08, 0x103E],
+    "series_number": [0x20, 0x11],
+    "protocol_name": [0x18, 0x1030],
+    "sequence_name": [0x18, 0x24]
+}
+SODict = {
+    "1": "sequential",
+    "2": "interleaved",
+    "4": "singleshot"
+}
+SSDict = {
+    "SE": "Spin Echo",
+    "IR": "Inversion Recovery",
+    "GR": "Gradient Recalled",
+    "EP": "Echo Planar",
+    "RM": "Research Mode"
+}
+SVDict = {
+    "SK": "segmented k-space",
+    "MTC": "magnetization transfer contrast",
+    "SS": "steady state",
+    "TRSS": "time reversed steady state",
+    "SP": "spoiled",
+    "MP": "MAG prepared",
+    "OSP": "oversampling phase",
+    "NONE": "no sequence variant"
+}
+ATDict = ["2D", "3D"]

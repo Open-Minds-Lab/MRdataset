dc2d421e5ba5cdafef6e9abc5a9189016b2f4818
Harsh Sinha <harsh.sinha@pitt.edu>
2022-07-08 16:39:18 -0400

    Add dictdiffer to requirements_dev.txt

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ with open('HISTORY.rst') as history_file:
 requirements = [
     'dicom2nifti',
     'nibabel',
-    'pydicom'
+    'pydicom',
+    'dictdiffer'
 ]
 
 test_requirements = ['pytest>=3', ]

d7af82fc6e8bae72f009758de1c66df53bb6ead4
Harsh Sinha <harsh.sinha@pitt.edu>
2022-07-08 12:23:13 -0400

    Adding docstrings and type hints in utils.py

--- MRdataset/utils.py
+++ MRdataset/utils.py
@@ -1,23 +1,41 @@
 import functools
-import sys
-import threading
 import time
 import uuid
-from collections import defaultdict
-from collections.abc import Hashable
-from collections.abc import Iterable
-
+from collections.abc import Hashable, Iterable
+from typing import List
 from dictdiffer import diff as dict_diff
 
 
-def safe_get(dictionary, keys, default=None):
+def safe_get(dictionary: dict, keys: str, default=None):
+    """
+    Used to get value from nested dictionaries without getting KeyError
+    @param dictionary: nested dict from which the value should be fetched
+    @param keys: string of keys delimited by '.'
+    @param default: if KeyError, return default
+    @return: object
+
+    Examples:
+    To get value, dictonary[tag1][tag2][tag3], if KeyError returns argument default
+    >>>     items = safe_get(dictionary, 'tags1.tag2.tag3')
+
+    """
     return functools.reduce(
         lambda d, key: d.get(key, default) if isinstance(d, dict) else default, keys.split("."),
         dictionary
     )
 
 
-def param_difference(dict1, dict2, ignore_params=None):
+def param_difference(dict1: dict, dict2: dict, ignore_params: Iterable = None) -> List[Iterable]:
+    """
+    A helper function to calculate differences between 2 dictionaries, dict1 and dict2. Returns
+    an iterator with differences between 2 dictionaries. The diff result consist of
+    multiple items, which represent addition/deletion/change and the item value is a deep copy
+    from the corresponding source and destination objects. See https://dictdiffer.readthedocs.io/en/latest/
+    @param dict1: source dict
+    @param dict2: destination dict
+    @param ignore_params: keys which should be ignored while calculating the difference
+    @return: list of items representing addition/deletion/change
+    """
     if isinstance(dict1, dict) and isinstance(dict2, dict):
         if ignore_params is None:
             return list(dict_diff(dict1, dict2))
@@ -28,83 +46,27 @@ def param_difference(dict1, dict2, ignore_params=None):
     raise TypeError("Expected type 'dict', got {} instead".format(type(dict2)))
 
 
-def fix(f):
+def random_name() -> str:
     """
-    Pythonic way to construct defaultdict() all the way down
-    See : https://quanttype.net/posts/2016-03-29-defaultdicts-all-the-way-down.html
-    d = fix(defaultdict)()
+    Function to generate a random identifier/name
+    @return: random_number cast to string
     """
-    return lambda *args, **kwargs: f(fix(f), *args, **kwargs)
-
-
-def flatten(arg):
-    returnlist = []
-    if not arg:
-        return returnlist
-    for i in arg:
-        if isinstance(i, list):
-            returnlist.extend(flatten(i))
-        else:
-            returnlist.append(i)
-    return returnlist
-
-
-class DeepDefaultDict(defaultdict):
-    def __init__(self, depth, default=list):
-        if depth > 1:
-            defaultdict.__init__(self, lambda: DeepDefaultDict(depth - 1, default))
-        else:
-            defaultdict.__init__(self, default)
-
-    def __repr__(self):
-        return dict.__repr__(self)
-
-    def __str__(self):
-        return dict.__str__(self)
-
-
-def random_name():
     return str(hash(str(uuid.uuid1())) % 1000000)
 
 
-def is_hashable(value):
+def is_hashable(value) -> bool:
+    """
+    Check if variable type is hashable, required to make dictionary keys
+    @param value: any variable
+    @return: bool
+    """
     return isinstance(value, Hashable)
 
 
-def timestamp():
+def timestamp() -> str:
+    """
+    Generates time string in the specified format
+    @return: time cast as string
+    """
     time_string = time.strftime("%m_%d_%Y_%H_%M")
     return time_string
-
-
-class Spinner:
-    busy = False
-    delay = 0.1
-
-    @staticmethod
-    def spinning_cursor():
-        while 1:
-            for cursor in '|/-\\':
-                yield cursor
-
-    def __init__(self, delay=None):
-        self.spinner_generator = self.spinning_cursor()
-        if delay and float(delay):
-            self.delay = delay
-
-    def spinner_task(self):
-        while self.busy:
-            sys.stdout.write(next(self.spinner_generator))
-            sys.stdout.flush()
-            time.sleep(self.delay)
-            sys.stdout.write('\b')
-            sys.stdout.flush()
-
-    def __enter__(self):
-        self.busy = True
-        threading.Thread(target=self.spinner_task).start()
-
-    def __exit__(self, exception, value, tb):
-        self.busy = False
-        time.sleep(self.delay)
-        if exception is not None:
-            return False

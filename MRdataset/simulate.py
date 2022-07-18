e879f15b00eca6eaad3deedd9b9a54c5398e2658
Harsh Sinha <harsh.sinha@pitt.edu>
2022-07-18 11:59:32 -0400

    Removed functions: make compliant/non_compliant test dataset

--- MRdataset/simulate.py
+++ MRdataset/simulate.py
@@ -46,73 +46,6 @@ def setup_directories(src):
     return src_dir, dest_dir
 
 
-def make_non_compliant_test_dataset(num_subjects,
-                                    num_compliant_subjects,
-                                    repetition_times,
-                                    echo_train_lengths,
-                                    flip_angles
-                                    ):
-    # src_dir, dest_dir = setup_directories(anon_data_dir) # noqa
-    dest_dir = make_compliant_test_dataset(num_compliant_subjects,
-                                           repetition_times[0],
-                                           echo_train_lengths[0],
-                                           flip_angles[0])
-
-    src_dir = Path(anon_data_dir)
-    dcm_list = list(src_dir.glob('**/*.dcm'))
-
-    compliant_subject_names = set([i.name for i in dest_dir.iterdir()])
-    non_compliant_subject_names = set()
-
-    i = 0
-    # Adding non_compliant subjects
-    while len(non_compliant_subject_names) < \
-        num_subjects - num_compliant_subjects:
-        filepath = dcm_list[len(dcm_list) - i - 1]
-        dicom = pydicom.read_file(filepath)
-        patient_id = str(dicom.PatientID)
-        if patient_id in compliant_subject_names:
-            i += 1
-            continue
-        dicom.RepetitionTime = repetition_times[1]
-        dicom.EchoTrainLength = echo_train_lengths[1]
-        dicom.FlipAngle = flip_angles[1]
-
-        export_file(dicom, filepath, dest_dir)
-        non_compliant_subject_names.add(str(patient_id))
-        i += 1
-    return dest_dir
-
-
-def make_compliant_test_dataset_w_info(num_subjects,
-                                       repetition_time,
-                                       echo_train_length,
-                                       flip_angle) -> Path:
-    src_dir, dest_dir = setup_directories()
-    dcm_list = list(src_dir.glob('**/*.dcm'))
-    dataset_info = defaultdict(lambda: defaultdict(dict))
-
-    subject_names = set()
-    i = 0
-    while len(subject_names) < num_subjects:
-        filepath = dcm_list[i]
-        dicom = pydicom.read_file(filepath)
-        patient_id = str(dicom.PatientID)
-
-        dicom.RepetitionTime = repetition_time
-        dicom.EchoTrainLength = echo_train_length
-        dicom.FlipAngle = flip_angle
-        modality = dicom.SeriesDescription.replace(' ', '_')
-        export_file(dicom, filepath, dest_dir)
-        subject_names.add(dicom.PatientID)
-
-        dataset_info[modality][str(patient_id)]['tr'] = repetition_time
-        dataset_info[modality][str(patient_id)]['etl'] = echo_train_length
-        dataset_info[modality][str(patient_id)]['fa'] = flip_angle
-        i += 1
-    return dest_dir, dataset_info
-
-
 def copyeverything(src, dst):
     if dst.exists():
         shutil.rmtree(dst)
@@ -166,8 +99,3 @@ def export_file(dicom, filepath, out_dir):
     output_path = out_dir / series_desc / patient_id
     output_path.mkdir(exist_ok=True, parents=True)
     dicom.save_as(output_path / filepath.name)
-
-
-if __name__ == '__main__':
-    test_data_path = make_test_dataset(3, 2000, 34.4, 80.0, 'ROW')
-    print(test_data_path)

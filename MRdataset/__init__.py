3ec7af3a3a7d6d881fd16841fb74d7d3d721de2d
Harsh Sinha <harsh.sinha@pitt.edu>
2022-09-09 12:27:05 -0400

    Add versioneer.py

--- MRdataset/__init__.py
+++ MRdataset/__init__.py
@@ -5,3 +5,6 @@ __email__ = 'raamana@gmail.com'
 __version__ = '0.1.0'
 
 from MRdataset.base import import_dataset
+
+from . import _version
+__version__ = _version.get_versions()['version']
--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@
 
 from setuptools import setup, find_packages
 
+import versioneer
+
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
@@ -49,7 +51,8 @@ setup(
     packages=find_packages(include=['MRdataset', 'MRdataset.*']),
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/openmindslab/MRdataset',
-    version='0.1.0',
+    url='https://github.com/Open-Minds-Lab/MRdataset',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     zip_safe=False,
 )

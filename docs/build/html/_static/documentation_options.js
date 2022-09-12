c7db666ca9a5cf4d1ce36a62cb359ddb3b3375dd
Harsh Sinha <harsh.sinha@pitt.edu>
2022-09-12 11:58:15 -0400

    Update docs with readme.html

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0+1.g7822222',
+    VERSION: '0.1.1+2.gd5fbd6a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

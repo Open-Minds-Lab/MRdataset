6c1a98224794e9426142e3e1a5e3ecdff93f4f12
Harsh Sinha <harsh.sinha@pitt.edu>
2022-11-22 15:32:22 -0500

    Function : Save_MR_dataset, saves pickle files

--- MRdataset/base.py
+++ MRdataset/base.py
@@ -801,3 +801,7 @@ def load_mr_dataset(filepath, style='dicom'):
             # If object is found, return object
             return fetched
 
+
+def save_mr_dataset(filename, folder, mrds_obj):
+    with open(folder / filename, "wb") as f:
+        pickle.dump(mrds_obj.__dict__, f)

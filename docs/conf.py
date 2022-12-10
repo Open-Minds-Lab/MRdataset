450a1d40a60fb6b6b996e894976aa5ba8cfa59d9
Harsh Sinha <harsh.sinha@pitt.edu>
2022-12-10 11:10:53 -0500

    Add support for mathjax, maybe required later

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,8 @@ import MRdataset
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
-              'sphinx.ext.napoleon']
+              'sphinx.ext.napoleon',
+              'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -168,5 +169,7 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
+# -- Options for MathJAX support ---------------------------------------
+mathjax_path = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
 
 

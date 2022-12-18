d3136fe5bbe5ba6946f485fa623d136441a83efb
Harsh Sinha <harsh.sinha@pitt.edu>
2022-12-18 15:40:48 -0500

    typo correction

--- MRdataset/utils.py
+++ MRdataset/utils.py
@@ -59,7 +59,7 @@ def safe_get(dictionary: dict, keys: str, default=None):
     Value stored in that key
 
     Examples:
-    To get value, dictonary[tag1][tag2][tag3],
+    To get value, dictionary[tag1][tag2][tag3],
     if KeyError: return default
     >>>     items = safe_get(dictionary, 'tags1.tag2.tag3')
 

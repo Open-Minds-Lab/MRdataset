daadca45cbf7bf5473f035c27825785c0a9fd91c
Harsh Sinha <harsh.sinha@pitt.edu>
2023-02-09 16:33:28 -0500

    version is extracted from _version.get_versions
    expose MRDS_EXT

--- MRdataset/__init__.py
+++ MRdataset/__init__.py
@@ -2,9 +2,10 @@
 
 __author__ = """Pradeep Raamana"""
 __email__ = 'raamana@gmail.com'
-__version__ = '0.1.0'
+# __version__ = '0.1.0'
 
 from .common import import_dataset, load_mr_dataset, save_mr_dataset
+from .utils import MRDS_EXT
 
 from . import _version
 __version__ = _version.get_versions()['version']

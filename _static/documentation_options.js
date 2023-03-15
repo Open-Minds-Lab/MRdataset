e688f5977162575a30c45cf883bae203f6dea197
Harsh Sinha <harsh.sinha@pitt.edu>
2023-03-15 11:38:56 -0400

    update docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.4+49.gda25889.dirty',
+    VERSION: '0+untagged.679.gb26d933',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

f39d7cb0fc623107ece2ca0cd98cc2f10623443c
Harsh Sinha <harsh.sinha@pitt.edu>
2023-03-15 12:12:47 -0400

    Remove dirty

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.679.gb26d933.dirty',
+    VERSION: '0+untagged.680.g1207d68',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

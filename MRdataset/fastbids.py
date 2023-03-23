e7953e2bbfa8e9d6abe6091356ac808637958baa
Harsh Sinha <harsh.sinha@pitt.edu>
2023-03-23 13:16:35 -0400

    check valid BIDS files, add suffixes to modality name, change file to filepath

--- MRdataset/fastbids.py
+++ MRdataset/fastbids.py
@@ -1,10 +1,11 @@
 """ FastBIDSDataset class to manage BIDS datasets without BIDSLayout object"""
 from pathlib import Path
 
-from MRdataset.base import BaseDataset, Run, Modality, Subject, Session
-from MRdataset.config import VALID_BIDS_EXTENSIONS, VALID_DATATYPES
+from MRdataset.base import BaseDataset, Modality, Subject, Session
+from MRdataset.bids_utils import parse, is_valid_bidsfile, combine_entity_labels
+from MRdataset.config import DatasetEmptyException
 from MRdataset.log import logger
-from MRdataset.utils import select_parameters, files_in_path, get_ext
+from MRdataset.utils import files_in_path
 
 
 # TODO: check what if each variable is None. Apply try catch
@@ -56,33 +57,31 @@ class FastBIDSDataset(BaseDataset):
         """
         # TODO: Need to handle BIDS datasets without JSON files
         for file in files_in_path(self.data_source):
-            ext = get_ext(file)
-            if ext in VALID_BIDS_EXTENSIONS:
+            if is_valid_bidsfile(file):
                 self.read_single(file)
         if not self.modalities:
             raise DatasetEmptyException
 
-    def read_single(self, file):
-        datatype = file.parent.name
-        if datatype not in VALID_DATATYPES:
-            return
-        modality_obj = self.get_modality_by_name(datatype)
-        if modality_obj is None:
-            modality_obj = Modality(datatype)
-        n_sess = file.parents[1].name
-        n_sub = file.parents[2].name
+    def read_single(self, filepath):
+        datatype = filepath.parent.name
+        n_sess = filepath.parents[1].name
+        n_sub = filepath.parents[2].name
         if 'sub' in n_sess:
             logger.info('Sessions dont exist')
             n_sess = 'ses-01'
-            n_sub = file.parents[1].name
+            n_sub = filepath.parents[1].name
+        modality_name = combine_entity_labels(filepath.stem, datatype)
+        modality_obj = self.get_modality_by_name(modality_name)
+        if modality_obj is None:
+            modality_obj = Modality(modality_name)
         subject_obj = modality_obj.get_subject_by_name(n_sub)
         if subject_obj is None:
             subject_obj = Subject(n_sub)
         session_node = subject_obj.get_session_by_name(n_sess)
         if session_node is None:
             session_node = Session(n_sess)
-        session_node = self.parse(session_node,
-                                  file)
+        session_node = parse(session_node,
+                             filepath)
         if session_node.runs:
             subject_obj.add_session(session_node)
         if subject_obj.sessions:

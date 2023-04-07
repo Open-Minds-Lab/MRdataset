121508a2d5648ec9d693855e5b6cd7f97ce84237
Pradeep Reddy Raamana <raamana@gmail.com>
2023-04-07 18:30:11 -0400

    style

--- MRdataset/config.py
+++ MRdataset/config.py
@@ -5,21 +5,21 @@ from pathlib import Path
 # Constant Dicom Identifiers Used for dataset creation and manipulation
 TAGS = {
     "series_instance_uid": (0x20, 0x0e),
-    "sequence": (0x18, 0x20),
-    "variant": (0x18, 0x21),
-    "patient_name": (0x10, 0x10),
-    "patient_id": (0x10, 0x20),
-    "study_id": (0x08, 0x1030),
-    "series_description": (0x08, 0x103E),
-    "series_number": (0x20, 0x11),
-    "protocol_name": (0x18, 0x1030),
-    "sequence_name": (0x18, 0x24),
-    "image_type": (0x08, 0x08),
-    "echo_number": (0x18, 0x86),
-    "te": [0x18, 0x81],
-    "patient_sex": [0x10, 0x40],
-    "patient_age": [0x10, 0x1010],
-}
+    "sequence"           : (0x18, 0x20),
+    "variant"            : (0x18, 0x21),
+    "patient_name"       : (0x10, 0x10),
+    "patient_id"         : (0x10, 0x20),
+    "study_id"           : (0x08, 0x1030),
+    "series_description" : (0x08, 0x103E),
+    "series_number"      : (0x20, 0x11),
+    "protocol_name"      : (0x18, 0x1030),
+    "sequence_name"      : (0x18, 0x24),
+    "image_type"         : (0x08, 0x08),
+    "echo_number"        : (0x18, 0x86),
+    "te"                 : [0x18, 0x81],
+    "patient_sex"        : [0x10, 0x40],
+    "patient_age"        : [0x10, 0x1010],
+    }
 
 # Constant Paths
 CACHE_DIR_NAME = ".mrdataset"
@@ -31,25 +31,25 @@ VALID_DATASET_STYLES = [
     'dicom',
     'bids',
     'fastbids',
-]
+    ]
 
 VALID_BIDS_EXTENSIONS = ['.json', '.nii', '.nii.gz']
 
 PARAMETER_NAMES = {
     # 'Manufacturer': [0x08, 0x70],
-    'BodyPartExamined': [0x18, 0x15],
-    'EchoTime': [0x18, 0x81],
-    'RepetitionTime': [0x18, 0x80],
-    'MagneticFieldStrength': [0x18, 0x87],
-    'FlipAngle': [0x18, 0x1314],
+    'BodyPartExamined'      : [0x18, 0x15],
+    'EchoTime'              : [0x18, 0x81],
+    'RepetitionTime'        : [0x18, 0x80],
+    'MagneticFieldStrength' : [0x18, 0x87],
+    'FlipAngle'             : [0x18, 0x1314],
     'PhaseEncodingDirection': [0x18, 0x1312],
-    'EchoTrainLength': [0x18, 0x0091],
-    'PixelBandwidth': [0x18, 0x95],
-    'ScanningSequence': [0x18, 0x20],
-    'SequenceVariant': [0x18, 0x21],
-    'MRAcquisitionType': [0x18, 0x23],
-    'PhaseEncodingSteps': [0x18, 0x89]
-}
+    'EchoTrainLength'       : [0x18, 0x0091],
+    'PixelBandwidth'        : [0x18, 0x95],
+    'ScanningSequence'      : [0x18, 0x20],
+    'SequenceVariant'       : [0x18, 0x21],
+    'MRAcquisitionType'     : [0x18, 0x23],
+    'PhaseEncodingSteps'    : [0x18, 0x89]
+    }
 
 datatypes = [
     'anat',
@@ -63,48 +63,48 @@ datatypes = [
     'micr',
     'perf',
     'pet'
-]
+    ]
 # Check  QualityControlSubject
 
 # Constant dicom Identifiers used to extract dicom headers
 HEADER_TAGS = {
-    "image_header_info": [0x29, 0x1010],
+    "image_header_info" : [0x29, 0x1010],
     "series_header_info": [0x29, 0x1020],
-}
+    }
 SLICE_MODE = {
     "1": "sequential",
     "2": "interleaved",
     "4": "singleshot"
-}
+    }
 SSDict = {
     "SE": "Spin Echo",
     "IR": "Inversion Recovery",
     "GR": "Gradient Recalled",
     "EP": "Echo Planar",
     "RM": "Research Mode"
-}
+    }
 SVDict = {
-    "SK": "segmented k-space",
-    "MTC": "magnetization transfer contrast",
-    "SS": "steady state",
+    "SK"  : "segmented k-space",
+    "MTC" : "magnetization transfer contrast",
+    "SS"  : "steady state",
     "TRSS": "time reversed steady state",
-    "SP": "spoiled",
-    "MP": "MAG prepared",
-    "OSP": "oversampling phase",
+    "SP"  : "spoiled",
+    "MP"  : "MAG prepared",
+    "OSP" : "oversampling phase",
     "NONE": "no sequence variant"
-}
+    }
 
 PAT = {
     "1": 'None',
     "2": 'Grappa',
     "3": 'Sense'
-}
+    }
 
 SHIM = {
     "1": 'tune_up',
     "2": 'standard',
     "4": 'advanced'
-}
+    }
 
 ATDict = ["2D", "3D"]
 
@@ -121,6 +121,7 @@ class MRException(Exception):
     in dicom file
     """
 
+
     def __init__(self, message, **kwargs):
         super().__init__(message)
 
@@ -130,6 +131,7 @@ class MRdatasetWarning(Exception):
     Custom error that is raised when some critical properties are not found
     in dicom file
     """
+
 
     def __init__(self, message, **kwargs):
         super().__init__(message)
@@ -149,6 +151,7 @@ class ChangingParamsInSeries(MRException):
     """Custom error that is raised when parameter values are different for
     different slices even though the SeriesInstanceUID is same."""
 
+
     def __init__(self, filepath):
         super().__init__("Expected all dicom slices to have same parameters. "
                          "Got changing parameters : {}".format(filepath))
@@ -157,6 +160,7 @@ class ChangingParamsInSeries(MRException):
 class SlicesNotStacked(MRException):
     """Custom error that is raised when parameter values are different for
     different slices even though the SeriesInstanceUID is same."""
+
 
     def __init__(self, filepath, parameter_name):
         super().__init__(
@@ -210,6 +214,7 @@ class MultipleProjectsInDataset(MRException):
     different study ids. Expected all dicom files belong to a single study
     id. Cross-Study is not supported.
     """
+
 
     def __init__(self, study_ids):
         super().__init__("Expected all dicom files to be in the same project"

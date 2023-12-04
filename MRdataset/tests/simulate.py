cd7addd3ad9a8f18368e04a310775a0fd2937f5e
Harsh Sinha <harsh.sinha@pitt.edu>
2023-12-03 20:54:46 -0500

    ci yml config

--- MRdataset/tests/simulate.py
+++ MRdataset/tests/simulate.py
@@ -1,5 +1,4 @@
 import errno
-import json
 import random
 import shutil
 import tempfile
@@ -7,14 +6,15 @@ import zipfile
 from collections import defaultdict
 from pathlib import Path
 
-import MRdataset.config
 import pydicom
-from MRdataset.tests.config import compl_data_xnat, \
-    compl_data_bids
+
+from MRdataset.tests.config import compl_data_xnat
 from MRdataset.utils import convert2ascii
-from bids import BIDSLayout
+
+# from bids import BIDSLayout
 
 THIS_DIR = Path(__file__).parent.resolve()
+
 
 def sample_dicom_dataset(tmp_path='/tmp'):
     DATA_ARCHIVE = THIS_DIR / 'resources/example_dicom_data.zip'
@@ -100,6 +100,7 @@ def make_multi_echo_dataset(num_subjects,
         i += 1
     return dest_dir
 
+
 def setup_directories(src):
     src_dir = Path(src).resolve()
     if not src_dir.exists():
@@ -175,72 +176,72 @@ def export_file(dicom, filepath, out_dir):
     dicom.save_as(output_path / filename)
 
 
-def make_bids_test_dataset(num_noncompliant_subjects,
-                           repetition_time,
-                           magnetic_field_strength,
-                           flip_angle):
-    src_dir, dest_dir = setup_directories(compl_data_bids)  # noqa
-    copyeverything(src_dir, dest_dir)
-    dataset_info = defaultdict(set)
-
-    layout = BIDSLayout(dest_dir.as_posix())
-    subjects = layout.get_subjects()
-
-    for i, modality in enumerate(MRdataset.config.VALID_DATATYPES):
-        count = num_noncompliant_subjects[i]
-        non_compliant_subjects = set()
-        for sub in subjects:
-            if count < 1:
-                break
-            filters = {'subject': sub,
-                       'datatype': modality,
-                       'extension': 'json'}
-            files = layout.get(**filters)
-            for bidsfile in files:
-                with open(bidsfile.path, "r") as read_file:
-                    parameters = json.load(read_file)
-                parameters['RepetitionTime'] = repetition_time
-                parameters['MagneticFieldStrength'] = magnetic_field_strength
-                parameters['FlipAngle'] = flip_angle
-                with open(bidsfile.path, 'w') as fh:
-                    json.dump(parameters, fh)
-            if files:
-                dataset_info[modality].add(sub)
-                if len(dataset_info[modality]) >= count:
-                    break
-    return dest_dir, dataset_info
-
-
-def make_toy_bids_dataset(path):
-    src_dir = Path(path).resolve()
-    if not src_dir.exists():
-        raise FileNotFoundError("input directory not found")
-    dest_dir = src_dir.parent / 'toy_dataset'
-
-    copyeverything(src_dir, dest_dir)
-
-    # Delete all nii.gz files
-    for filepath in dest_dir.glob('**/*.nii.gz'):
-        filepath.unlink()
-
-    # Delete all tsv files
-    for filepath in dest_dir.glob('**/*.tsv'):
-        filepath.unlink()
-
-    # Delete a folder named 'sourcedata'. Not required
-    # shutil.rmtree(Path(dest_dir/'sourcedata')
-
-    # Create new subjects
-    # for k in range(2):
-    subjects = [f for f in dest_dir.iterdir() if f.name.startswith("sub")]
-    for i, subject in enumerate(subjects):
-        new_sub_name = '-'.join(
-            ['sub', str(len(subjects) + i + 1).zfill(2)])
-        for session in subject.iterdir():
-            for datatype in session.iterdir():
-                for file in datatype.iterdir():
-                    name = str(file.name).split('_')[1:]
-                    new_filename = '_'.join([new_sub_name] + name)
-                    out_path = dest_dir / new_sub_name / session.name / datatype.name
-                    out_path.mkdir(parents=True, exist_ok=True)
-                    shutil.copy(file, out_path / new_filename)
+# def make_bids_test_dataset(num_noncompliant_subjects,
+#                            repetition_time,
+#                            magnetic_field_strength,
+#                            flip_angle):
+#     src_dir, dest_dir = setup_directories(compl_data_bids)  # noqa
+#     copyeverything(src_dir, dest_dir)
+#     dataset_info = defaultdict(set)
+#
+#     layout = BIDSLayout(dest_dir.as_posix())
+#     subjects = layout.get_subjects()
+#
+#     for i, modality in enumerate(MRdataset.config.VALID_DATATYPES):
+#         count = num_noncompliant_subjects[i]
+#         non_compliant_subjects = set()
+#         for sub in subjects:
+#             if count < 1:
+#                 break
+#             filters = {'subject': sub,
+#                        'datatype': modality,
+#                        'extension': 'json'}
+#             files = layout.get(**filters)
+#             for bidsfile in files:
+#                 with open(bidsfile.path, "r") as read_file:
+#                     parameters = json.load(read_file)
+#                 parameters['RepetitionTime'] = repetition_time
+#                 parameters['MagneticFieldStrength'] = magnetic_field_strength
+#                 parameters['FlipAngle'] = flip_angle
+#                 with open(bidsfile.path, 'w') as fh:
+#                     json.dump(parameters, fh)
+#             if files:
+#                 dataset_info[modality].add(sub)
+#                 if len(dataset_info[modality]) >= count:
+#                     break
+#     return dest_dir, dataset_info
+#
+#
+# def make_toy_bids_dataset(path):
+#     src_dir = Path(path).resolve()
+#     if not src_dir.exists():
+#         raise FileNotFoundError("input directory not found")
+#     dest_dir = src_dir.parent / 'toy_dataset'
+#
+#     copyeverything(src_dir, dest_dir)
+#
+#     # Delete all nii.gz files
+#     for filepath in dest_dir.glob('**/*.nii.gz'):
+#         filepath.unlink()
+#
+#     # Delete all tsv files
+#     for filepath in dest_dir.glob('**/*.tsv'):
+#         filepath.unlink()
+#
+#     # Delete a folder named 'sourcedata'. Not required
+#     # shutil.rmtree(Path(dest_dir/'sourcedata')
+#
+#     # Create new subjects
+#     # for k in range(2):
+#     subjects = [f for f in dest_dir.iterdir() if f.name.startswith("sub")]
+#     for i, subject in enumerate(subjects):
+#         new_sub_name = '-'.join(
+#             ['sub', str(len(subjects) + i + 1).zfill(2)])
+#         for session in subject.iterdir():
+#             for datatype in session.iterdir():
+#                 for file in datatype.iterdir():
+#                     name = str(file.name).split('_')[1:]
+#                     new_filename = '_'.join([new_sub_name] + name)
+#                     out_path = dest_dir / new_sub_name / session.name / datatype.name
+#                     out_path.mkdir(parents=True, exist_ok=True)
+#                     shutil.copy(file, out_path / new_filename)
